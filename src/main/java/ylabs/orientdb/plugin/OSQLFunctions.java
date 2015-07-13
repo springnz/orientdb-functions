55cec6149325e1adbf48d82dafeb0994ba28d077
Mike Ourednik <mike@spring.co.nz>
2015-07-13 13:50:28 +1200

    v1.1.0. Implemented pow function (code copied from orientdb plugin example).

--- src/main/java/ylabs/orientdb/plugin/OSQLFunctions.java
+++ src/main/java/ylabs/orientdb/plugin/OSQLFunctions.java
@@ -1,47 +1,16 @@
 package ylabs.orientdb.plugin;
 
 import com.orientechnologies.common.log.OLogManager;
-import com.orientechnologies.orient.core.command.OCommandContext;
-import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.sql.OSQLEngine;
-import com.orientechnologies.orient.core.sql.functions.OSQLFunctionAbstract;
-
-import java.util.Date;
+import ylabs.orientdb.plugin.functions.DateTimePlusSecondsFunction;
+import ylabs.orientdb.plugin.functions.PowFunction;
 
 public class OSQLFunctions {
 
     public static void registerPlugins(Object iRequestor) {
-        OSQLEngine.getInstance().registerFunction("dateTimePlusSeconds", OSQLFunctions.dateTimeAddSecondsFunction());
+        OSQLEngine.getInstance().registerFunction("dateTimePlusSeconds", new DateTimePlusSecondsFunction());
         OLogManager.instance().info(iRequestor, "dateTimePlusSeconds function registered");
-    }
-
-    public static OSQLFunctionAbstract dateTimeAddSecondsFunction() {
-      return new OSQLFunctionAbstract("dateTimePlusSeconds", 2, 2) {
-          public Object execute(
-                  Object iThis, OIdentifiable iCurrentRecord, Object iCurrentResult,
-                  Object[] iParams, OCommandContext iContext) {
-
-              if (iParams[0] == null || iParams[1] == null) {
-                  return null;
-              }
-
-              if (!(iParams[0] instanceof Date) || !(iParams[1] instanceof Number)) {
-                  return null;
-              }
-
-              final Date timestamp = (Date) iParams[0];
-              final Number seconds = (Number) iParams[1];
-              timestamp.setTime(timestamp.getTime() + seconds.longValue() * 1000);
-              return timestamp;
-          }
-
-          public String getSyntax() {
-              return "dateTimePlusSeconds(<datetime>, <seconds>)";
-          }
-
-          public boolean aggregateResults() {
-              return false;
-          }
-      };
+        OSQLEngine.getInstance().registerFunction("pow",  new PowFunction());
+        OLogManager.instance().info(iRequestor, "pow function registered");
     }
 }
---
+++ src/main/java/ylabs/orientdb/plugin/functions/PowFunction.java
@@ -0,0 +1,90 @@
+package ylabs.orientdb.plugin.functions;
+
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.sql.functions.OSQLFunctionAbstract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowFunction extends OSQLFunctionAbstract {
+
+    public PowFunction() {
+        super("pow", 2, 3);
+    }
+
+    public Object execute(Object iThis, OIdentifiable iCurrentRecord, Object iCurrentResult,
+                          final Object[] iParams, OCommandContext iContext) {
+        if (iParams[0] == null || iParams[1] == null) {
+            return null;
+        }
+        if (!(iParams[0] instanceof Number) || !(iParams[1] instanceof Number)) {
+            return null;
+        }
+
+        final long base = ((Number) iParams[0]).longValue();
+        final long power = ((Number) iParams[1]).longValue();
+
+        if (iParams.length == 3) { // modular exponentiation
+            if (iParams[2] == null) {
+                return null;
+            }
+            if (!(iParams[2] instanceof Number)) {
+                return null;
+            }
+
+            final long mod = ((Number) iParams[2]).longValue();
+            if (power < 0) {
+                OLogManager.instance().warn(this, "negative numbers as exponent are not supported");
+            }
+            return modPow(base, power, mod);
+        }
+
+        return power > 0 ? pow(base, power) : 1D / pow(base, -power);
+    }
+
+    public String getSyntax() {
+        return "pow(<base>, <power> [, <mod>])";
+    }
+
+    public boolean aggregateResults() {
+        return false;
+    }
+
+    private double pow(long base, long power) {
+        double r = 1;
+        List<Boolean> bits = bits(power);
+        for (int i = bits.size() - 1; i >= 0; i--) {
+            r *= r;
+            if (bits.get(i)) {
+                r *= base;
+            }
+        }
+
+        return r;
+    }
+
+    private double modPow(long base, long power, long mod) {
+        double r = 1;
+        List<Boolean> bits = bits(power);
+        for (int i = bits.size() - 1; i >= 0; i--) {
+            r = (r * r) % mod;
+            if (bits.get(i)) {
+                r = (r * base) % mod;
+            }
+        }
+
+        return r;
+    }
+
+    private List<Boolean> bits(long n) {
+        List<Boolean> bits = new ArrayList<Boolean>();
+        while (n > 0) {
+            bits.add(n % 2 == 1);
+            n /= 2;
+        }
+
+        return bits;
+    }
+}
